013810f9d173bd1689335cf6e9751230114594ff
Alex Stakh <olstakh@microsoft.com>
2020-11-17 15:23:02 -0800

    oops

--- tests/Server/Server.Tests.fs
+++ tests/Server/Server.Tests.fs
@@ -8,23 +8,7 @@ open Server
 
 open StackExchange.Redis
 
-let server = testList "Server" [
-    testCase "Redis" <| fun _ ->
-        let connection = ConnectionMultiplexer.Connect("safeshortlink.redis.cache.windows.net:6380,password=t5Q2H4iLpyoV+vwwpgvld6xxhoZuVtXuN1pMDvDxgZM=,ssl=True,abortConnect=False")
-        let cache : IDatabase = connection.GetDatabase()
-        let response = cache.StringGet(RedisKey("Hello1"))
-        Expect.isTrue (response.IsNull) "empty redis"
-
-    testCase "Redis get and set" <| fun _ ->
-        let connection = ConnectionMultiplexer.Connect("safeshortlink.redis.cache.windows.net:6380,password=t5Q2H4iLpyoV+vwwpgvld6xxhoZuVtXuN1pMDvDxgZM=,ssl=True,abortConnect=False")
-        let cache : IDatabase = connection.GetDatabase()
-        
-        let r1 = cache.StringSet(RedisKey("Hello2"), RedisValue("Privet"))
-        Expect.isTrue r1 "Redis value was expected to be set"
-
-        let response = cache.StringGet(RedisKey("Hello2"))
-        Expect.isFalse (response.IsNull) "empty redis"
-]
+let server = testList "Server" []
 
 let all =
     testList "All"
